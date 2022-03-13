209478e1a61abb985535fbe0ebd4d49a023d93c4
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-03-13 09:22:57 -0700

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /* Given Code, don't edit */
+document.querySelector("button").addEventListener("click", handleClick);
 
 document.querySelector("button").addEventListener("click", handleClick);
 
@@ -9,5 +10,19 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  document.getElementById("greeting").textContent = msg;
+}
+
+/* Write your implementation of greet() */
+function greet(timeStr) {
+  const hour = parseInt(timeStr, 10);
+  if
+   (timeStr < 0 || timeStr > 24)
+    return 'Please enter a valid response'
+     else
+      if ( hour < 12) return "Good Morning"
+      if ( hour > 17) return "Good Evening"
+      return "Good Afternoon"
+}
